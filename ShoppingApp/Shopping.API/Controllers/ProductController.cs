1addfd56c538f5e488cf7e455e1df32fda2c3308
kostaskps <kapsalisk.develop@hotmail.com>
2021-02-09 12:09:47 +0200

    Add product model and controller in API

---
+++ ShoppingApp/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] Brands = new[]
+        {
+            "MSI", "Asus", "Gigabyte", "Gainward", "Sapphire", "EVGA"
+        };
+
+        private static readonly string[] ModelNames = new[]
+        {
+            "GeForce GTX 1030", "GeForce GTX 1650", "GeForce RTX 2060", "GeForce RTX 2070"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ProductController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductModel> Get()
+        {
+            var random = new Random();
+            
+            return Enumerable.Range(1, 5).Select(index => new ProductModel
+            {
+                Id = index,
+                Brand = Brands[random.Next(Brands.Length)],
+                ModelName = ModelNames[random.Next(ModelNames.Length)],
+                Category = "Graphics Cards",
+                Price = random.Next(88, 289)
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ShoppingApp/Shopping.API/Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.API.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
