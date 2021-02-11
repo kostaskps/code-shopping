6af47cba087553c78e3f2ea66eefac08125b582a
kostaskps <kapsalisk.develop@hotmail.com>
2021-02-11 20:06:50 +0200

    Add support to run microservices with multi-containers using Docker-Compose

--- ShoppingApp/Shopping.App/Startup.cs
+++ ShoppingApp/Shopping.App/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Shopping.App
 {
@@ -25,7 +21,8 @@ namespace Shopping.App
         {
             services.AddHttpClient("APIClient", configureClient =>
             {
-                configureClient.BaseAddress = new Uri("http://localhost:5000/");
+                //configureClient.BaseAddress = new Uri("http://localhost:5000/"); // API Url
+                configureClient.BaseAddress = new Uri(Configuration["Shopping_API_Service_URL"]);
             });
             services.AddControllersWithViews();
         }
